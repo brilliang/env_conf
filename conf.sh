f5d47cd0ccbdc8f185c8223ac14bb6218c9e80a1
zhuliang <azhu1123@sina.cn>
2016-04-11 11:51:48 +0800

    add many echo in conf.sh

--- conf.sh
+++ conf.sh
@@ -12,6 +12,7 @@ if [ "$(ls -A $GIT_CONF_DEP/dougblack)" ]; then
   echo 'submodule already init.'
 else
   git submodule update --init --recursive
+  echo 'submodule update OK'
 fi
 
 cd ~
@@ -19,18 +20,18 @@ HOME_DIR=`pwd`
 ORIG_CONF_BCK="$HOME_DIR/conf.orig"
 
 if [ -d $ORIG_CONF_BCK ];then
-  echo $ORIG_CONF_BCK' exist. pls clean it.'
+  echo '$ORIG_CONF_BCK exist. pls clean it.'
   exit 1
 fi
 
 # make the deposit for old conf files
 mkdir -p $ORIG_CONF_BCK
-echo mkdir -p $ORIG_CONF_BCK
+echo "mkdir -p $ORIG_CONF_BCK"
 
 mvf () {
   if [ -e $1 ];then
     mv $1 $2
-    echo mv $1 $2
+    echo "mv $1 $2"
   else
     echo "$1 not exist"
   fi
@@ -38,24 +39,28 @@ mvf () {
 
 cd $HOME_DIR/.oh-my-zsh/
 git reset --hard origin/master
+echo "reset .oh-my-zsh"
 
 rm $HOME_DIR/.oh-my-zsh/themes/crcandy.zsh-theme # 可以通过git reset 找到rm掉的文件
 cp $GIT_CONF_DEP/crcandy.zsh-theme $HOME_DIR/.oh-my-zsh/themes/crcandy.zsh-theme
+echo "set crcandy.zsh-theme"
 
-# move original configuration file into ~/conf.orig/ directory
 for p in .zshrc .aliasrc .hashrc .screenrc .pathrc .gitconfig .vimrc .vim/ .viminfo
 do
   mvf $HOME_DIR/$p $ORIG_CONF_BCK
 done
+echo "move original configuration file into ~/conf.orig/ directory"
 
-# copy new conf files from git deposit
 for p in .zshrc .aliasrc .hashrc .screenrc .pathrc .gitconfig .vimrc .vim/
 do
   cp -rf $GIT_CONF_DEP/$p $HOME_DIR
 done
+echo "copy new conf files from git deposit"
 
 source $HOME_DIR/.zshrc
+
 # 安装vundle管理的vim插件
+echo "start set vundle plugins"
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 if [ -d ~/.vim/bundle/YouCompleteMe ];then
@@ -75,4 +80,5 @@ done
 
 cd $HOME_DIR/.oh-my-zsh
 git commit -am 'zl modified.'
+echo "All jobs done."
 set +e
