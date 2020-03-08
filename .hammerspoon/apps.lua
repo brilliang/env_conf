7677c306ac7172a7845e7de9f75b9d59615f7e05
zhuliang <azhu1123@sina.cn>
2020-03-08 17:05:04 +0000

    add more apps control

--- .hammerspoon/apps.lua
+++ .hammerspoon/apps.lua
@@ -2,7 +2,14 @@ apps = {
     {{"alt"}, "F", "Finder"},
     {{"alt"}, "S", "Safari"},
     {{"alt"}, "C", "Google Chrome"},
+    {{"alt"}, "O", "Microsoft Outlook"},
+
     {{"alt"}, "T", "iTerm"},
+    {{"alt"}, "L", "Sublime Text"},
+    {{"alt"}, "V", "VS Code @ FB - Insiders"},
+
+    {{"shift", "cmd"}, "W", "Workplace Chat"},
+    {{"shift", "cmd"}, "P", "WhatsApp"},
 }
 for i = 1, #apps do
     hs.hotkey.bind(apps[i][1], apps[i][2], function() hs.application.open(apps[i][3]) end)
