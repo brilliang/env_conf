96cb651b51d22dd8e6d21c1abc67bf0f73b44601
zhuliang <azhu1123@sina.cn>
2020-04-13 00:35:43 +0100

    hide win when it's focused already.

--- .hammerspoon/apps.lua
+++ .hammerspoon/apps.lua
@@ -6,12 +6,22 @@ apps = {
 
     {{"alt"}, "T", "iTerm"},
     {{"alt"}, "L", "Sublime Text"},
+    {{"alt"}, "D", "Dash"},
     {{"alt"}, "V", "VS Code @ FB - Insiders"},
+    {{"alt", "ctrl"}, "Q", "Quip"},
 
-    {{"shift", "cmd"}, "W", "Workplace Chat"},
-    {{"shift", "cmd"}, "P", "WhatsApp"},
+    {{"alt", "ctrl"}, "W", "Workplace Chat"},
+    {{"alt", "ctrl"}, "P", "WhatsApp"},
 }
+
 for i = 1, #apps do
-    hs.hotkey.bind(apps[i][1], apps[i][2], function() hs.application.open(apps[i][3]) end)
+    hs.hotkey.bind(apps[i][1], apps[i][2], function() 
+        focusd_app = hs.window.focusedWindow():application()
+        if focusd_app == nil or focusd_app:name() ~= apps[i][3] then
+            hs.application.open(apps[i][3]) 
+        else
+            focusd_app:hide()
+        end
+    end)
 end
 
